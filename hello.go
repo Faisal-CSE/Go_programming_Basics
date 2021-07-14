37a5ac7b3bac4f83be0c5f749e55501a383683c2
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2021-07-14 15:23:00 +0600

    Create hello.go

---
+++ hello.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var num1 int
+	// var num2 int
+	// num1 = 15
+	// num2 = 8
+	// var result = num1 + num2
+
+	// var num1 = 15
+	// var num2 = 8
+	// var result = num1 + num2
+
+	var num1, num2 int
+
+	num1, num2 = 2, 3
+	var result = num1 + num2
+
+	result__new := 6 //Means >> var result__new = 6 || create and assign a variable. This is short form
+
+	fmt.Println("Summation is: ")
+	fmt.Println(result)
+	fmt.Println(result__new)
+
+	fmt.Println("-------------------")
+
+	var name = "Faisal Porag"
+	fmt.Print(name)
+}
