67c552aca6e723488cd03cf5c814d5518a03d47d
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2021-07-14 15:56:22 +0600

    Update basic_function.go

--- basic_function.go
+++ basic_function.go
@@ -12,11 +12,24 @@ func add2(x, y int) int { //If all the parameters are same type
 	return output
 }
 
+//Return multiple values
+func calc(x, y int) (int, int) {
+	var output1 = x + y
+	var output2 = x - y
+
+	return output1, output2
+}
+
 func main() {
 	num1 := 5
 	num2 := 6
 
-	result := add(num1, num2)
+	num3 := 10
+	num4 := 4
 
+	result := add(num1, num2)
 	fmt.Println("The result is: ", result)
+
+	result1, result2 := calc(num3, num4)
+	fmt.Println("The result1 is: ", result1, " || The result2 is: ", result2)
 }
