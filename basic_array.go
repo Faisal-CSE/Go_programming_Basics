1067820efccc10d468633ecd953a9a6e7c77289e
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2021-07-14 17:29:35 +0600

    Create basic_array.go

---
+++ basic_array.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a [5]int
+
+	fmt.Println("Elements: ", a)
+
+	a[3] = 12
+	a[4] = 10
+
+	fmt.Println("Elements: ", a)
+
+	fmt.Println("Get index 4 data: ", a[4])
+
+	fmt.Println("Length of array: ", len(a))
+
+	//-------------------------------
+
+	arr__elements := [5]int{1, 3, 5, 7, 9}
+	fmt.Println("Array is : ", arr__elements)
+
+	for i := 0; i < len(arr__elements); i++ {
+		if arr__elements[i] == 3 {
+			arr__elements[i] = 500 //Value change using condition
+		}
+		fmt.Println("Serial is: ", arr__elements[i])
+	}
+}
