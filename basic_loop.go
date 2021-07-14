2951b4906abf2ecd2b8aa43f39b32388d0ae4ced
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2021-07-14 15:38:19 +0600

    Create basic_loop.go

---
+++ basic_loop.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//1st way
+	// i := 1
+	// for i <= 5 {
+	// 	fmt.Println("Hello", i)
+	// 	i++
+	// }
+
+	//2nd Way
+	for i := 1; i <= 10; i++ {
+		fmt.Println("Hello ", i)
+	}
+}
